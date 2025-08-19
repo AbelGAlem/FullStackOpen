384b5f1fd56a5fbc24a301ce78eea0519ee6f13a
AbelGAlem <abelzeraftechnologies@gmail.com>
2025-08-19 21:44:51 +0300

    Part 1 (1.1 - 1.5) Course Information Exercise Submission

---
+++ part1/courseInfo/README.md
@@ -0,0 +1,1 @@
+Part 1 (1.1 - 1.5) Course Information Exercise Submission
---
+++ part1/courseInfo/src/App.jsx
@@ -0,0 +1,63 @@
+// note: destructured props for readability, hope that isnt an issue
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Total = ({parts}) => {
+  //const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  let total = 0
+  parts.forEach((part) => 
+    total += part.exercises
+  );
+  return (
+    <p>Number of excercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
