24a675758d2b7d1df2b58d4adaed5724c01dfd09
AbelGAlem <abelzeraftechnologies@gmail.com>
2026-01-07 20:40:21 +0200

    Part 6 (6.9)

--- part6/redux-anecdotes-main/src/App.jsx
+++ part6/redux-anecdotes-main/src/App.jsx
@@ -1,9 +1,11 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import AnecdoteFilter from './components/AnecdoteFilter'
 
 const App = () => {
   return (
     <div>
+      <AnecdoteFilter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
@@ -2,7 +2,13 @@ import { useDispatch, useSelector } from "react-redux"
 import { addVote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => [...state].sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(({anecdotes, filter}) => {
+    if(filter === '') {
+      return anecdotes
+    }
+    return anecdotes.filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+  })
+  
   const dispatch = useDispatch()
 
   const vote = id => {
