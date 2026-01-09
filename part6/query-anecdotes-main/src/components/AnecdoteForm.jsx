9bc87244f38398ccf60da9cf6c42f50ccd9250e6
AbelGAlem <abelzeraftechnologies@gmail.com>
2026-01-09 21:06:43 +0200

    Part 6 (6.20 - 6.24) Final

---
+++ part6/query-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+import { useContext } from 'react'
+import NotificationContext from '../notificationContext'
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const {notificationDispatch} = useContext(NotificationContext)
+
+  const newMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+      notificationDispatch({ type: 'SET', payload: `anecdote '${newAnecdote.content}' created` })
+    },
+    onError: (error) => {
+      notificationDispatch({ type: 'SET', payload: error.message })
+    }
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newMutation.mutate( content )
+  }
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
